72f0c3ae621767fc24a957d80a6e7fe0ce3a309e
davidpacco <david.j.pacco@gmail.com>
2024-06-26 20:11:42 -0500

    Initial commit

---
+++ src/components/Notification.js
@@ -0,0 +1,12 @@
+export function Notification() {
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+  return (
+    <div style={style}>
+      render here notification...
+    </div>
+  )
+}
