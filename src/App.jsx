8503849e4db3cc11dccad3013e85db47ea8cb04b
davidpacco <david.j.pacco@gmail.com>
2024-07-01 09:10:55 -0500

    Add exercise 6.12 - better anecdotes step 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { AnecdoteForm } from './AnecdoteForm'
 import { AnecdoteList } from './components/AnecdoteList'
 import { Filter } from './components/Filter'
+import { Notification } from './components/Notification'
 
 function App() {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Notification />
       <Filter />
       <AnecdoteList />
       <AnecdoteForm />
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,12 +1,17 @@
+import { useSelector } from "react-redux"
+
 export function Notification() {
+  const notification = useSelector(({ notification }) => notification)
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
+
   return (
     <div style={style}>
-      render here notification...
+      {notification}
     </div>
   )
 }
